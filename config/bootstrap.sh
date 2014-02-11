7b6d1153b01886686f3523bdabd3b7915d849e07
Oliver Steele <steele@osteele.com>
2014-02-11 09:43:39 -0500

    reverse proxy /fingerboard to s3 bucket

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -eu
 
-apt-get update
+apt-get update -qq
 apt-get install -y apache2
+
+echo 'ServerName blog.osteele.dev' > /etc/apache2/httpd.conf
+
 a2enmod rewrite
 a2enmod actions
+a2enmod proxy
+a2enmod proxy_http
+a2enmod -q headers
 
-rm -f /etc/apache2/sites-enabled/000-default
-ln -fs /vagrant/config/code.osteele.com.conf /etc/apache2/sites-enabled/
+a2dissite default
+
 ln -fs /vagrant/build /var/www/code.osteele.com
+ln -fs /vagrant/config/code.osteele.com.conf /etc/apache2/sites-available/code.osteele.com
+a2ensite code.osteele.com
 
 service apache2 restart
