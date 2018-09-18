ead1577b1fd5c45e1a40fe23f03863afcd30b025
Oliver Steele <steele@osteele.com>
2018-09-18 11:59:57 -0400

    Override repo metadata

--- scripts/collect_repos.rb
+++ scripts/collect_repos.rb
@@ -3,9 +3,11 @@
 require 'fileutils'
 require 'graphql/client'
 require 'graphql/client/http'
+require 'yaml'
 
 SCHEMA_PATH = '.cache/github-schema.json'
 OUTPUT_PATH = 'data/repos.json'
+REPO_OVERRIDE_PATH = 'data/repo-overrides.yaml'
 
 HTTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
   def headers(_context)
@@ -82,27 +84,36 @@ cursor = nil
 viewer = nil
 loop do
   result = Client.query(IndexQuery, variables: { count: 100, cursor: cursor })
-  # owners = [result.data.viewer.login, 'olin-computing', 'olin-build', 'mlsteele']
   viewer = result.data.viewer
   repo_count += viewer.repositories.edges.length
   repos += viewer.repositories.edges
     .map(&:node)
     .reject do |repo|
-      repo.is_fork || repo.is_private # || !owners.include?(repo.owner.login)
+      repo.is_fork || repo.is_private
     end.map do |repo|
       h = repo.to_h.dup
-      h['primaryLanguage'] = repo.primary_language.name if repo.primary_language
       h['languages'] = repo.languages.edges.map { |edge| edge.node.name }
+      h['primaryLanguage'] = repo.primary_language.name if repo.primary_language
       h['topics'] = repo.repository_topics.edges.map { |edge| edge.node.topic.name }
       h.delete 'homepageUrl' if h['homepageUrl']&.empty?
       h.delete 'isFork'
       h.delete 'isPrivate'
       h.delete 'repositoryTopics'
+      h['owner'] = h['owner'].dup
+      h['owner'].delete '__typename'
       h
     end
   page_info = viewer.repositories.page_info
   break unless page_info.has_next_page
   cursor = page_info.end_cursor
+end
+
+# Apply overrides
+overrides = YAML.load(File.read REPO_OVERRIDE_PATH)
+repos.each do |repo|
+  over = overrides[repo['nameWithOwner']]
+  next unless over
+  repo['tags'] = over['tags'] if over.key? 'tags'
 end
 
 puts "Writing #{repos.length} public source repos / #{repo_count} total"
