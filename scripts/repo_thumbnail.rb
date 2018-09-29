c460017b342c0d0b5a485dac706427befe3f6c16
Oliver Steele <steele@osteele.com>
2018-09-29 08:47:24 -0400

    Get README content from initial API call

--- scripts/repo_thumbnail.rb
+++ scripts/repo_thumbnail.rb
@@ -1,31 +1,41 @@
 # frozen_string_literal: true
 
+require 'base64'
+
 # Match an image path in Markdown
 # TODO: look for <img src=…> too
 MD_IMAGE_RE = /\!\[[^\]]*\]\((.+?)\)/
 
 GITHUB_USER_AGENT = 'osteele/code.osteele.com'
 
-# Given a GitHub nameWithOwner, return it's README's download URL, or nil
-def get_repo_readme_url(nwo)
+# Given a GitHub nameWithOwner, return its README info or nil
+def get_repo_readme_data(nwo)
   uri = URI.join("https://api.github.com/repos/#{nwo}/readme")
   headers = { 'User-Agent' => GITHUB_USER_AGENT }
   token = ENV['JEKYLL_GITHUB_TOKEN']
   headers['Authorization'] = "Bearer #{token}" if token
   begin
-    return JSON.parse(uri.open(headers).read)['download_url']
+    data = JSON.parse(uri.open(headers).read)
   rescue OpenURI::HTTPError
     return nil
   end
+  raise EncodingError("Unknown encoding: #{data['encoding']}") \
+    unless data['encoding'] == 'base64'
+
+  return {
+    content: Base64.decode64(data['content']),
+    download_url: data['download_url'],
+    name: data['name']
+  }
 end
 
 # Give a the Markdown source of a README (or other file), return its content —
 # skipping over an initial header, and any initial images (which tend to
 # be badges instead of logos or screenshots).
 def get_markdown_content(markdown)
-  markdown
+  return markdown
 end
 
 def get_markdown_images(markdown)
-  markdown.scan(MD_IMAGE_RE)
+  return markdown.scan(MD_IMAGE_RE).map(&:first)
 end
--- spec/repo_thumbnail_spec.rb
+++ spec/repo_thumbnail_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative '../scripts/repo_thumbnail'
 
-RSpec.describe :get_repo_readme_url do
+RSpec.describe :get_repo_readme_data do
   it 'returns the README from a GitHub repo'
   it "handles repos that don't have READMEs"
   it 'passes non-404 exceptions'
@@ -15,12 +15,12 @@ end
 RSpec.describe :get_markdown_images do
   it 'recognizes Markdown image markup' do
     images = get_markdown_images('line 1\npre ![](path/to/image.png) post')
-    expect(images).to eq [['path/to/image.png']]
+    expect(images).to eq ['path/to/image.png']
   end
 
   it 'recognizes images with alt text' do
     images = get_markdown_images('line 1\npre ![alt text](path/to/image.png) post')
-    expect(images).to eq [['path/to/image.png']]
+    expect(images).to eq ['path/to/image.png']
   end
 
   it 'recognizes HTML image markup'
