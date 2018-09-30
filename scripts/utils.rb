3114523a506e710f65ae79e02f59dac112e2521b
Oliver Steele <steele@osteele.com>
2018-09-30 11:38:01 -0400

    Blacklist repos

--- scripts/utils.rb
+++ scripts/utils.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 require "json"
+require "yaml"
 
-JSON_OPTIONS = { indent: "  ", space: " ", array_nl: "\n", object_nl: "\n" }
-               .freeze
+JSON_OPTIONS = \
+  {
+    indent: "  ",
+    space: " ",
+    array_nl: "\n",
+    object_nl: "\n"
+  }.freeze
 
 def read_json(fname)
-  JSON.parse(File.open(fname).read)
+  return JSON.parse(File.open(fname).read)
 end
 
 # TODO: use atomic_write
@@ -14,4 +20,10 @@ def write_json(fname, json)
   File.open(fname, "w") do |f|
     f << JSON.generate(json, **JSON_OPTIONS)
   end
+end
+
+def load_yaml(fname)
+  return YAML.safe_load(File.read(fname))
+rescue Errno::ENOENT
+  return {}
 end
